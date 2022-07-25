31e002136e429f7d46a1d0290130426f217ce634
Prawito <prawito.id@gmail.com>
2022-07-25 19:38:24 +0700

    fix test App.js & Data NavLinks

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   const { theme } = useContext(ThemeContext);
   return (
-    <BrowserRouter basename="web-landing">
+    <BrowserRouter basename="">
       <ThemeProvider theme={{ theme }}>
         <GlobalStyles />
         <RouteSite />
