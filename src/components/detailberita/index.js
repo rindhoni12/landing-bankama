abf10a5b80cddc2c93822d0f3db009ca290df6f4
Prawito <prawito.id@gmail.com>
2022-07-27 19:12:42 +0700

    fix test breadcumbs at pages berita

--- src/components/detailberita/index.js
+++ src/components/detailberita/index.js
@@ -22,10 +22,10 @@ const DetailBeritaComponents = ({ newItem }) => {
                   <nav className="berita_breadcrum">
                     <ol className="breadcrum">
                       <li className="breadcrum_item">
-                        <a href="/web-landing">Home</a>
+                        <a href="/">Home</a>
                       </li>
                       <li className="breadcrum_item">
-                        <a href="/web-landing/berita-kami">Berita</a>
+                        <a href="/berita-kami">Berita</a>
                       </li>
                       <li className="breadcrum_item">
                         <p>{itemBerita.judul}</p>
