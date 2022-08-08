2b8aefbc19a909625b09ec258f1baf54b65f2c43
Prawito <prawito.id@gmail.com>
2022-08-08 19:25:41 +0700

    fix test pages form-penyaluran and change url package

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,7 @@ import { GlobalStyles, ThemeContext } from "../config";
 function App() {
   const { theme } = useContext(ThemeContext);
   return (
-    <BrowserRouter basename="">
+    <BrowserRouter basename="web-landing">
       <ThemeProvider theme={{ theme }}>
         <GlobalStyles />
         <RouteSite />
